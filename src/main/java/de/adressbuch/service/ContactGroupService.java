7b89429d633dee5bc13334d911b7232038c1c685
Junaed Tariq <juta00004@htwsaar.de>
2026-02-06 02:44:22 +0100

    Removed unnecessary extra checks

--- src/main/java/de/adressbuch/service/ContactGroupService.java
+++ src/main/java/de/adressbuch/service/ContactGroupService.java
@@ -19,44 +19,25 @@ public class ContactGroupService {
     }
 
     public void addContactToGroup(String contactId, String groupId) {
-        if (contactExists(contactId) && groupExists(groupId)) {
-            contactGroupRepository.addContactToGroup(contactId, groupId);
-            logger.info("Kontakt {} zu Gruppe {} geaddet", contactId, groupId);
-        } else {
-            throw new IllegalArgumentException("Contact or Group does not exist");
-        }
+        contactGroupRepository.addContactToGroup(contactId, groupId);
+        logger.info("Kontakt {} zu Gruppe {} geaddet", contactId, groupId);
     }
 
     public boolean isContactInGroup(String contactId, String groupId) {
-        if (contactExists(contactId) && groupExists(groupId)) {
-            boolean foundContactCheck = contactGroupRepository.isContactInGroup(contactId, groupId);
-            logger.debug("Kontakt {} in Gruppe {} gesucht: {}", contactId, groupId, foundContactCheck);
-            return foundContactCheck;
-        } else {
-            logger.warn("Kontakt oder Gruppe existiert nicht: {} / {}", contactId, groupId);
-            throw new IllegalArgumentException("Contact or Group does not exist");
-        }
+        boolean foundContactCheck = contactGroupRepository.isContactInGroup(contactId, groupId);
+        logger.debug("Kontakt {} in Gruppe {} gesucht: {}", contactId, groupId, foundContactCheck);
+        return foundContactCheck;
     }
 
     public List<String> findContactIdsByGroupId(String groupId) {
-        if (groupExists(groupId)) {
-            List<String> contactIds = contactGroupRepository.findContactIdsByGroupId(groupId);
-            logger.debug("{} Kontakte in Gruppe {} gefunden", contactIds.size(), groupId);
-            return contactIds;
-        } else {
-            logger.warn("Gruppe existiert nicht: {}", groupId);
-            throw new IllegalArgumentException("Contact or Group does not exist");
-        }
+        List<String> contactIds = contactGroupRepository.findContactIdsByGroupId(groupId);
+        logger.debug("{} Kontakte in Gruppe {} gefunden", contactIds.size(), groupId);
+        return contactIds;
     }
 
     public void removeContactFromGroup(String contactId, String groupId) {
-        if (contactExists(contactId) && groupExists(groupId)) {
-            contactGroupRepository.removeContactFromGroup(contactId, groupId);
-            logger.info("Kontakt {} von Gruppe {} entfernt", contactId, groupId);
-        } else {
-            logger.warn("Kontakt oder Gruppe existiert nicht: {} / {}", contactId, groupId);
-            throw new IllegalArgumentException("Contact or Group does not exist");
-        }
+        contactGroupRepository.removeContactFromGroup(contactId, groupId);
+        logger.info("Kontakt {} von Gruppe {} entfernt", contactId, groupId);
     }
 
     public boolean groupExists(String id) {
